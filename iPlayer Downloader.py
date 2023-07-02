b0c0642f979b15d05853ecb75c58b04e1cef2592
Drew Cameron <8572599+TheMaster870@users.noreply.github.com>
2023-07-02 11:54:27 +0100

    Version 2

--- iPlayer Downloader.py
+++ iPlayer Downloader.py
@@ -1,39 +1,90 @@
 import os
 
-print("Please enter the list of PIDs, one on each line.")
-print("Enter 'done' when finished")
-pids = []
-finished = False
-while finished == False:
-    entry = input()
-    if entry == "done":
-        finished = True
+def PIDDownloader():
+    print("Please enter the list of PIDs, one on each line.")
+    print("Enter 'done' when finished")
+    pids = []
+    finished = False
+    while finished == False:
+        entry = input()
+        if entry == "done":
+            finished = True
+        else:
+            if entry != "":
+                pids.append(entry)
+
+    if len(pids) > 0:
+        os.system("cls")
+        print("I will download the following PIDs:")
+        pidlist = ""
+        for pid in pids:
+            pidlist = pidlist + pid + ", "
+        print(pidlist)
+        print("")
+        print("Is this correct? y or n")
+        correct = input()
+        correct = correct.lower()
+        if correct == "y" or correct == "yes":
+            os.system("cls")
+            i = 1
+            total = len(pids)
+            for pid in pids:
+                print("Downloading episode " + str(i) + " of " + str(total) + "...")
+                command = "get_iplayer --overwrite --force --tv-quality=fhd --pid=" + pid
+                os.system(command)
+                print("")
+                i += 1
+            
+            print("#######################")
+            print("Completed")
+        else:
+            print("Chose no")
+
     else:
-        pids.append(entry)
+        print("No PIDs entered")
 
-os.system("cls")
-print("I will download the following PIDs:")
-pidlist = ""
-for pid in pids:
-    pidlist = pidlist + pid + ", "
-print(pidlist)
-print("")
-print("Is this correct? y or n")
-correct = input()
-if correct != "y":
-    print("Exiting")
-    exit
+    print("ENTER to continue")
+    input()
 
-os.system("cls")
-i = 1
-total = len(pids)
-for pid in pids:
-    print("Downloading episode " + str(i) + " of " + str(total) + "...")
-    command = "get_iplayer --overwrite --force --tv-quality=fhd --pid=" + pid
-    os.system(command)
+def PIDInfo():
+    print("Please enter the list of PIDs, one on each line.")
+    print("Enter 'done' when finished")
+    pids = []
+    finished = False
+    while finished == False:
+        entry = input()
+        if entry == "done":
+            finished = True
+        else:
+            pids.append(entry)
+
+    if len(pids) > 0:
+        i = 1
+        total = len(pids)
+        for pid in pids:
+            print("Getting episode info " + str(i) + " of " + str(total) + "...")
+            command = "get_iplayer -i --pid=" + pid
+            os.system(command)
+            print("")
+            i += 1
+            
+        print("#######################")
+        print("Completed")
+    else:
+        print("No PIDs entered")
+    
+    print("ENTER to continue")
+    input()
+    
+
+while(True):
+    os.system("cls")
+    print("iPlayer Downloader")
     print("")
-    i += 1
-
-print("#######################")
-print("Done")
-input()
+    print("1 - PID Downloader")
+    print("2 - PID Info")
+    choice = input()
+    if choice == "1":
+        PIDDownloader()
+    elif choice == "2":
+        PIDInfo()
